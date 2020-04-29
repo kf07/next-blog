fcf8bbc57c5af58741d5eb0bd7e98e16e07d8d18
kango furusu <furusu0715@gmail.com>
2020-04-29 13:48:57 +0900

    commit test

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>{siteTitle} React</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
